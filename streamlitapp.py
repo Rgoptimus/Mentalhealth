445742f468072902cff51348b93618b43153b52d
Irvan Zidny <74554600+Rgoptimus@users.noreply.github.com>
2024-08-12 14:27:43 +0700

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,11 +1,14 @@
-# example/st_app_gsheets_using_service_account.py
-
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-st.title("Read Google Sheet as DataFrame")
+url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
-df = conn.read(worksheet="Example 1")
 
-st.dataframe(df)
+data = conn.read(spreadsheet=url, usecols=[0, 1])
+st.dataframe(data)
+
+st.title("🎈 My new app")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
