38ed220a15aab390ac77750591a60380b540ce8b
Irvan Zidny <74554600+Rgoptimus@users.noreply.github.com>
2024-08-12 16:58:01 +0700

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,14 +1,15 @@
+# Import the Streamlit library
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
 
-url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
+# Set the title of the app
+st.title('Simple Streamlit App')
 
-conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+# Create a text input box
+user_input = st.text_input("Enter your name:")
 
-data = conn.read(spreadsheet=url, usecols=[0, 1])
-st.dataframe(data)
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Create a button that updates the app when clicked
+if st.button('Submit'):
+    if user_input:
+        st.write(f"Hello, {user_input}!")
+    else:
+        st.write("Please enter your name.")
